a298da7bf4c37415d362ca1e8abe317dfd74a5ae
Abuzar <kerimovabuzer25@gmail.com>
2023-04-21 20:51:14 +0400

    implemented first feature

--- src/az/ingress/git/Application.java
+++ src/az/ingress/git/Application.java
@@ -3,5 +3,7 @@ package az.ingress.git;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Starting development phase");
+
+        First.implementation();
     }
 }
---
+++ src/az/ingress/git/First.java
@@ -0,0 +1,7 @@
+package az.ingress.git;
+
+public class First {
+    public static void implementation() {
+        System.out.println("First feature implemented");
+    }
+}
